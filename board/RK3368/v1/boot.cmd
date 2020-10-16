5f08aaad75916887e38d688458c98d56afa03830
jesse <mr.bossman075@gmail.com>
2020-10-16 12:37:14 -0400

    linux boots aarch64 cant do zimage (self extracting image) fixed folder name and used rockchips defconfig

--- board/RK3368/v1/boot.cmd
+++ board/RK3368/v1/boot.cmd
@@ -1,4 +1,4 @@
 setenv bootargs fbcon=map:10  fbcon=font:MINI4x6 console=ttyS0,115200 panic=5 rootwait root=/dev/mmcblk0p2 rw 
-load mmc 0:1 0x80C00000 RK3368_linux.dtb
-load mmc 0:1 0x80008000 zImage
-bootz 0x80008000 - 0x80C00000
+load mmc 0:1 0x2200000 RK3368_linux.dtb
+load mmc 0:1 0x200000 Image
+booti 0x200000 - 0x2200000
